f0ca6bc04116c5dcfa0e02b6ca83ab18b1fe4ea6
Pulkit <kpulkit15234@gmail.com>
2024-01-31 12:17:01 +0530

    export fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Typewriter from "./Typewriter.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Typewriter onComplete={()=>console.log("complete")} />
-);
+export * from "./Typewriter";
